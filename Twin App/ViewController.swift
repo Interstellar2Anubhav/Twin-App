06d9a1a0ea86f4f49abfd104e408534cca7c233c
Interstellar2Anubhav <dennisthecelebutante@gmail.com>
2021-05-18 00:55:29 +0530

    Initial Commit

--- Twin App/ViewController.swift
+++ Twin App/ViewController.swift
@@ -7,13 +7,86 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var sliderCollectionView: UICollectionView!
+    
+    @IBOutlet weak var pageView: UIPageControl!
+    
+    var imgArr = [  UIImage(named:"DC1"),
+                    UIImage(named:"Beast1") ,
+                    UIImage(named:"Narnia1") ,
+                    UIImage(named:"StarWars1") ,
+                    UIImage(named:"XMEN1") ,
+                    UIImage(named:"DC2") ,
+                    UIImage(named:"Hogwarts") ,
+                    UIImage(named:"Narnia2") ,
+                    UIImage(named:"StarWars2") ,
+                    UIImage(named:"XMEN2") ]
+    
+    var timer = Timer()
+    var counter = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        pageView.numberOfPages = imgArr.count
+        pageView.currentPage = 0
+        DispatchQueue.main.async {
+            self.timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(self.changeImage), userInfo: nil, repeats: true)
+        }
     }
-
-
+    
+    @objc func changeImage() {
+     
+     if counter < imgArr.count {
+         let index = IndexPath.init(item: counter, section: 0)
+         self.sliderCollectionView.scrollToItem(at: index, at: .centeredHorizontally, animated: true)
+         pageView.currentPage = counter
+         counter += 1
+     } else {
+         counter = 0
+         let index = IndexPath.init(item: counter, section: 0)
+         self.sliderCollectionView.scrollToItem(at: index, at: .centeredHorizontally, animated: false)
+         pageView.currentPage = counter
+         counter = 1
+     }
+         
+     }
 }
 
+
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return imgArr.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        if let vc = cell.viewWithTag(111) as? UIImageView {
+            vc.image = imgArr[indexPath.row]
+        }
+        return cell
+    }
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let size = sliderCollectionView.frame.size
+        return CGSize(width: size.width, height: size.height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0.0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0.0
+    }
+}
